9ad0330a9569dc98c03e6b18721aa93162db7b99
Rushikesh petkar <rushikesh.petkar.etc@ghrce.raisoni.net>
2023-10-09 22:48:36 +0530

    this is a task1 of codsoft

---
+++ lib/constants/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+const Color tdRed = Color(0xFFDA4040);
+const Color tdBlue = Color(0xFFEEEB52);
+
+const Color tdBlack = Color(0xFF9ACCCA);
+const Color tdGrey = Color(0xFFF6E6E6);
+
+const Color tdBGColor = Color(0xFF305252);
